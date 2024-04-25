ae090921b86b0a30a05bca7d0247fbc07765e178
arielpd02 <143342070+arielpd02@users.noreply.github.com>
2024-04-25 00:53:17 +0000

    Done CP2

--- Clase Práctica 4 - SIMD-20240417/entregable/simd-bundle.v0.2/src/ej1y2/checkpoint2.asm
+++ Clase Práctica 4 - SIMD-20240417/entregable/simd-bundle.v0.2/src/ej1y2/checkpoint2.asm
@@ -1,6 +1,7 @@
 ; En .rodata declaro las mascaras que voy a utilizar
 section .rodata: 	
-shuffle_h1: times 4 db 3 	;La mascara queda en posiciones contiguas de memoria
+align 16					;Alineamos antes de declarar en memoria
+shuffle_h1: times 4 db 3 	;La mascara queda en posiciones contiguas de memoria , se lee al reves x el endianness
 shuffle_h2: times 4 db 7
 shuffle_h3: times 4 db 11
 shuffle_h4: times 4 db 15
@@ -21,27 +22,27 @@ four_of_a_kind_asm:
 
 	xor rax,rax 				; rax(=count):=0
 	mov ecx,esi					; rcx:= n (cant. de manos)
-	shr rcx,2					; Divido por 4 / itero n/4 veces
+	shr ecx,2					; Divido por 4 / itero n/4 veces
 
-	movdqu xmm7,[shuffle_h1]	; Bajo la mascara de shift a xmm7
 	movdqu xmm6,[wipe_suit]		; Bajo la mascara de wipe-suit a xmm6
+	movdqu xmm7,[shuffle_h1]	; Bajo la mascara de shift a xmm7
 	movdqu xmm5,[count_hands]	; Bajo la mascara de '1s' a xmm5
 	xor r8,r8					; r8(=partial_count):= 0
 
 .ciclo:
 	;Limpiamos el suit de cada carta , nos molesta para hacer comparaciones entre packed data
 	movdqu xmm0,[rdi]	; xmm0:= M33 | M32 | M31 | M30 | M23 |.........| M10 | M03 | M02 | M01 | M00 (MANOxCARTAy) -> 16 cartas, 4 manos
-	psrlw xmm0,4		; Desplazo 4 bits a derecha en cada word para luego aplicar mascara para wipear el suit de c/carta
-	pand xmm0,xmm6		; xmm0:= 0V15 | 0V14 | .	.	.	| 0V1 | 0V0 -> En cada carta/byte nos queda solo el valor
+	pand xmm0,xmm6		; xmm0:= 0V15 | 0V14 | .	.	.	| 0V1 | 0V0 -> En cada carta/byte nos queda solo el valor puesto que la rep. era [suit|value]
 
-	;Pintamos el c/mano del valor de la carta en el byte + significativo de esta
+	;Pintamos c/mano del valor de la carta en el byte + significativo de esta
 	movdqu xmm1,xmm0 	; Copio las cartas en xmm1 y trabajo con este registro
 	pshufb xmm1,xmm7	; xmm1:= v15 | v15 | v15 | v15 | v14 | v14 | ... | v0 | v0 | v0 | v0 
-
+	
 	;Comparamos con mascara de '1s' para filtrar manos que no cumplen
-	pcmpeqb xmm1,xmm0	; Comparo con los valores originales -> 0xffffffff si las cartas de una mano tienen el mismo valor
-	pcmpeqw xmm1,xmm5	; Comparo de a doublewords -> 0xffffffff si la mano cumple , todo 0 sino  
+	pcmpeqb xmm1,xmm0	; Comparo con los valores originales -> 0xff ff ff ff si las cartas de una mano tienen el mismo valor
+	pcmpeqd xmm1,xmm5	; Comparo de a doublewords -> 0xffffffff si la mano cumple , todo 0 sino  
 	pabsd xmm1,xmm1		; Hago el valor absoluto , luego en cada dw/mano me queda 1 si cumple, 0 sino
+
 
 	;Sumamos las manos obtenidas en xmm1
 	phaddd xmm1,xmm1
